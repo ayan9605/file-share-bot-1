13a48af047bc063d1c55b639e2db1ad5ee8d5d1b
ayan9605 <sumit334434@gmail.com>
2025-07-24 15:28:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 BOT_TOKEN = os.getenv("7877987202:AAHXA_ekAy-y7u7OQ-p-DKsihd-u9HNYg6g") or "7877987202:AAHXA_ekAy-y7u7OQ-p-DKsihd-u9HNYg6g"
 SUPABASE_URL = os.getenv("https://itczznbyvtjgphvpczui.supabase.co/") or "https://itczznbyvtjgphvpczui.supabase.co/"
-SUPABASE_KEY = os.getenv("SUPABASE_KEY") or "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0Y3p6bmJ5dnRqZ3BodnBjenVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNDg4OTAsImV4cCI6MjA2ODkyNDg5MH0.Ugk1WQ_UcWaRKawoLhJ7dCMIlPSzJgNUYLg5Q6Snp5U"
-CHANNEL_ID = int(os.getenv("CHANNEL_ID") or "-1001234567890")
+SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0Y3p6bmJ5dnRqZ3BodnBjenVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNDg4OTAsImV4cCI6MjA2ODkyNDg5MH0.Ugk1WQ_UcWaRKawoLhJ7dCMIlPSzJgNUYLg5Q6Snp5U") or "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0Y3p6bmJ5dnRqZ3BodnBjenVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNDg4OTAsImV4cCI6MjA2ODkyNDg5MH0.Ugk1WQ_UcWaRKawoLhJ7dCMIlPSzJgNUYLg5Q6Snp5U"
+CHANNEL_ID = int(os.getenv("-1002806108952") or "-1002806108952")
