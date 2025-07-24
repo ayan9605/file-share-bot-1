5f35b0b15f7130a79b9b895f4199c37086f9aff0
ayan9605 <sumit334434@gmail.com>
2025-07-24 15:14:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN") or "YOUR_TELEGRAM_BOT_TOKEN"
-SUPABASE_URL = os.getenv("SUPABASE_URL") or "https://your-project.supabase.co"
-SUPABASE_KEY = os.getenv("SUPABASE_KEY") or "your-supabase-anon-key"
+BOT_TOKEN = os.getenv("7877987202:AAHXA_ekAy-y7u7OQ-p-DKsihd-u9HNYg6g") or "7877987202:AAHXA_ekAy-y7u7OQ-p-DKsihd-u9HNYg6g"
+SUPABASE_URL = os.getenv("https://itczznbyvtjgphvpczui.supabase.co/") or "https://itczznbyvtjgphvpczui.supabase.co/"
+SUPABASE_KEY = os.getenv("SUPABASE_KEY") or "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0Y3p6bmJ5dnRqZ3BodnBjenVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNDg4OTAsImV4cCI6MjA2ODkyNDg5MH0.Ugk1WQ_UcWaRKawoLhJ7dCMIlPSzJgNUYLg5Q6Snp5U"
 CHANNEL_ID = int(os.getenv("CHANNEL_ID") or "-1001234567890")
